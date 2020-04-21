7e8a99f472510e3f3a16d91b1e4af420be135aea
Glitch (hello-express) <none>
2020-04-21 18:37:13 +0000

    🎯🏄 Checkpoint ./server.js:15261052/136

--- server.js
+++ server.js
@@ -5,10 +5,17 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const app = express();
+const pug = require('pug');
+
+app.use()
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
   response.send('I love CodersX');
+});
+
+app.get('/todos', (request, response) => {
+  response.send('');
 });
 
 // listen for requests :)
